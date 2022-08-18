ce789ce512e71d6b8a5ab3b3dc68e7d4b7487c19
tp <61617660+ittp@users.noreply.github.com>
2022-08-19 02:06:29 +0300

    Create wsl.setup.cmd

---
+++ windows/wsl.setup.cmd
@@ -0,0 +1,7 @@
+echo "-- start --"
+
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+wsl --set-default-version 2
+
+echo "-- end --"
