79fc99071c4c74ea6c86cf37845c8047e734766b
tp <61617660+ittp@users.noreply.github.com>
2022-08-20 16:03:18 +0300

    Update lib-jq.sh

--- examples/lib-jq.sh
+++ examples/lib-jq.sh
@@ -2,3 +2,6 @@
 
 echo '{"user": {"id": 1, "name": "Cameron"}}' | jq '{ name: .user.name }' 
 # { "name": "Cameron" }
+
+
+jq -r '[(.dependencies, .devDependencies) | keys] | flatten | length' package.json
